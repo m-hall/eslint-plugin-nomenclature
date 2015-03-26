91d630a801dd3d5296d0680e5dc264fbf8c8259c
Michael Hall <github@michaelhall.ca>
2015-03-26 13:42:50 -0400

    Add prefix-scope rule.
    This rule will require a prefix on local scope variables after a certain
    function scope level is achieved.
    This is obviously not perfect as the goal is to deal with singletons
    differently than regular functions, but it's probably the safest way.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 module.exports = {
     rules: {
         "prefix-parameters": require("./lib/rules/prefix-parameters"),
-        "prefix-globals": require("./lib/rules/prefix-globals")
+        "prefix-globals": require("./lib/rules/prefix-globals"),
+        "prefix-scope": require("./lib/rules/prefix-scope")
     }
 };
