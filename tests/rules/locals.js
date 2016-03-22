3dcf0ee347557e8041d7552dd9ac534b3159c2c9
Volker Buzek <volker.buzek@js-soft.com>
2016-03-22 15:40:53 +0100

    locals check implementation, unit tests, devDependency
    - add mocha as a devDependency in package.json
    - removed eslint-tester b/c of deprecated development
    - fix: implementation of "locals"-rules backward-compatible (retain string-checks)
    - ported unit tests for locals-rules to ESLint.RuleTester
    - additional unit tests

--- tests/rules/locals.js
+++ tests/rules/locals.js
@@ -1,34 +1,36 @@
-'use strict';
+"use strict";
 
-var linter = require('eslint').linter,
-    ESLintTester = require('eslint-tester');
-var eslintTester = new ESLintTester(linter);
+var rule = require("../../lib/rules/locals"),
+    RuleTester = require("eslint").RuleTester;
 
-eslintTester.addRuleTest('./lib/rules/locals', {
+var ruleTester = new RuleTester();
+ruleTester.run("locals", rule, {
     valid: [
-        { code: 'function name(){ var local; }', args: [1, ''] },
-        { code: 'function name(){ var _local; }', args: [1, '_'] },
-        { code: 'function name(){ var __local; }', args: [1, '__'] },
-        { code: 'function name(){ var $local; }', args: [1, '$'] },
-        { code: 'function name(){ var __local, __local2; }', args: [1, '__'] },
-        { code: 'function name(){ var _local, __local2; }', args: [1, '_'] },
-        { code: 'function name(){ var local; function name2(){ var _local2; }}', args: [1, {'prefix': '_', 'level': 2}] }
+        { code: 'function name(){ var local; }', options: [''] },
+        { code: 'function name(){ var _local; }', options: ['_'] },
+        { code: 'function name(){ var __local; }', options: ['__'] },
+        { code: 'function name(){ var $local; }', options: ['$'] },
+        { code: 'function name(){ var __local, __local2; }', options: ['__'] },
+        { code: 'function name(){ var _local, __local2; }', options: ['_'] },
+        { code: 'function name(){ var local; function name2(){ var _local2; }}', options: [{'prefix': '_', 'level': 2}] },
+        { code: 'function name(){ var _level1; function name2(){ var _local2, aArray, bBool, cChar; }}', options: [{'prefix': ['_','a','b','c'], 'level': 1}] },
+        { code: 'function name(){ var level2Only; function name2(){ var _local2, aArray, bBool, cChar; }}', options: [{'prefix': ['_','a','b','c'], 'level': 2}] }
     ],
 
     invalid: [
         {
             code: 'function name(){ var local; }',
-            args: [1, '_'],
+            options: ['_'],
             errors: [ { message: 'Local variable "local" is not prefixed with "_".' } ]
         },
         {
             code: 'function name(){ var _local, local2; }',
-            args: [1, '_'],
+            options: ['_'],
             errors: [ { message: 'Local variable "local2" is not prefixed with "_".' } ]
         },
         {
             code: 'function name(){ var _local, local2; }',
-            args: [1, '__'],
+            options: ['__'],
             errors: [
                 { message: 'Local variable "_local" is not prefixed with "__".' },
                 { message: 'Local variable "local2" is not prefixed with "__".' }
@@ -36,13 +38,23 @@ eslintTester.addRuleTest('./lib/rules/locals', {
         },
         {
             code: 'function name(){ var $local; }',
-            args: [1, '_'],
+            options: ['_'],
             errors: [ { message: 'Local variable "$local" is not prefixed with "_".' } ]
         },
         {
             code: 'function name(){ var local; function name2(){ var local2; }}',
-            args: [1, {'prefix': '_', 'level': 2}],
+            options: [{'prefix': '_', 'level': 2}],
             errors: [ { message: 'Local variable "local2" is not prefixed with "_".' } ]
+        },
+        {
+            code: 'function name(){ var noprefix_from_array; }',
+            options: [{'prefix': ['_','a','b','c']}],
+            errors: [ { message: 'Local variable "noprefix_from_array" is not prefixed with any of "_,a,b,c".' } ]
+        },
+        {
+            code: 'function name(){ var _level2; function name2(){ var noprefix_from_array_level2_only; }}',
+            options: [{'prefix': ['_','a','b','c']}],
+            errors: [ { message: 'Local variable "noprefix_from_array_level2_only" is not prefixed with any of "_,a,b,c".' } ]
         }
     ]
 });
